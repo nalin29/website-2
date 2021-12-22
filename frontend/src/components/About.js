1394d907b7b0fc89a6c22111742f6d588e63c78e
Nalin Mahajan <nalinmahajan@outlook.com>
2021-12-21 22:11:01 -0600

    fixed line break on mobile

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,9 +5,9 @@ export default function About() {
          <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                  Hello, I'm Nalin.
-                  <br className="hidden lg:inline-block" />
-                  An aspiring Software Engineer
+                  Hello, I'm Nalin. 
+                  <br className="lg:inline-block" />
+                   An aspiring Software Engineer
                </h1>
                <p className="mb-8 leading-relaxed">
                   As a CS student at UT who is interested in solving the problems of today and 
